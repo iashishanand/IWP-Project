d3a0b50bb255946eb6a2f42248b7adb598b29e54
Ashish <lashishanandl@gmail.com>
2022-09-15 23:20:50 +0530

    Review-1 files merge

---
+++ script/scriptsanchit.js
@@ -0,0 +1,24 @@
+function replace1() {
+    document.getElementById("Movies").style.display="block";
+    document.getElementById("Documentary").style.display="none";
+    document.getElementById("Sports").style.display="none";
+    document.getElementById("TvShows").style.display="none";
+    }
+    function replace2() {
+    document.getElementById("Movies").style.display="none";
+    document.getElementById("Documentary").style.display="none";
+    document.getElementById("Sports").style.display="none";
+    document.getElementById("TvShows").style.display="block";
+    }
+    function replace3() {
+    document.getElementById("Movies").style.display="none";
+    document.getElementById("TvShows").style.display="none";
+    document.getElementById("Sports").style.display="none";
+    document.getElementById("Documentary").style.display="block";
+    }
+    function replace4() {
+    document.getElementById("Movies").style.display="none";
+    document.getElementById("TvShows").style.display="none";
+    document.getElementById("Documentary").style.display="none";
+    document.getElementById("Sports").style.display="block";
+    }
